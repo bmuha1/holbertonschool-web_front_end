0a6854cbfb2c6b2723392702a4d3b2298e37ea9b
Brent Muha <brent.muha@gmail.com>
2020-09-21 14:16:29 -0700

    Remove stray comma

---
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -0,0 +1,12 @@
+let user = {
+  hobby: 'Calligraphy',
+  favoriteSport: 'Hockey',
+  astrologicalSign: 'Aries',
+  firstName: 'Buillaume',
+  lastName: 'Ialva',
+  location: 'Telaviv',
+  occupation: 'Engineer',
+};
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+boundGetArea();
